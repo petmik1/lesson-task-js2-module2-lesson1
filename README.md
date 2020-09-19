3d0d5a6dc925fe490f450bed0ddb336afe85146b
cnnrbrn <connorbobrien@gmail.com>
2020-09-19 12:34:35 +0200

    Add first files

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript 2 - Module 2
+
+## Lesson Task 1 Question
+
+Using the array in `script.js`, create a list from each item displaying the `name` and `rating` property for each.
+
+Clicking on an item should toggle a class called `played` on each item.
---
+++ script.js
@@ -0,0 +1,7 @@
+const games = [
+    { id: 324, name: "Great game", rating: 5.0 },
+    { id: 124, name: "Ok game", rating: 3.0 },
+    { id: 678, name: "Meh game", rating: 2.5 },
+    { id: 28, name: "Terrible game", rating: 1.0 },
+    { id: 4, name: "Mediocre game", rating: 3.0 },
+];

8eac29cba2571d353896af9ab428c9e8bf0a7304
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2022-11-01 00:28:55 +0100

    worked on the task

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const result = document.querySelector(".gamesList");
+
 const games = [
     { id: 324, name: "Great game", rating: 5.0 },
     { id: 124, name: "Ok game", rating: 3.0 },
@@ -5,3 +7,20 @@ const games = [
     { id: 28, name: "Terrible game", rating: 1.0 },
     { id: 4, name: "Mediocre game", rating: 3.0 },
 ];
+
+games.forEach (createList)
+
+function createList(Element) {
+ console.log(Element)
+ result.innerHTML +=`
+    <li>${Element.name} Rating: ${Element.rating}</li>
+ `
+}
+const items = document.querySelectorAll("li");
+items.forEach(function(item){
+    item.addEventListener("click", toggleClass);
+});
+
+function toggleClass(event){
+    event.target.classList.toggle("played");
+}
